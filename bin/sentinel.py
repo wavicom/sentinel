429ca05ed7f4e74036968c7953c0115a48bc537f
wavicom <40639092+wavicom@users.noreply.github.com>
2018-06-28 19:38:13 +0500

    Update sentinel.py

--- bin/sentinel.py
+++ bin/sentinel.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), '../lib
 import init
 import config
 import misc
-from dashd import DashDaemon
+from wavid import WaviDaemon
 from models import Superblock, Proposal, GovernanceObject, Watchdog
 from models import VoteSignals, VoteOutcomes, Transient
 import socket
@@ -19,22 +19,22 @@ from scheduler import Scheduler
 import argparse
 
 
-# sync dashd gobject list with our local relational DB backend
-def perform_dashd_object_sync(dashd):
-    GovernanceObject.sync(dashd)
+# sync wavid gobject list with our local relational DB backend
+def perform_wavid_object_sync(wavid):
+    GovernanceObject.sync(wavid)
 
 
 # delete old watchdog objects, create new when necessary
-def watchdog_check(dashd):
+def watchdog_check(wavid):
     printdbg("in watchdog_check")
 
     # delete expired watchdogs
-    for wd in Watchdog.expired(dashd):
+    for wd in Watchdog.expired(wavid):
         printdbg("\tFound expired watchdog [%s], voting to delete" % wd.object_hash)
-        wd.vote(dashd, VoteSignals.delete, VoteOutcomes.yes)
+        wd.vote(wavid, VoteSignals.delete, VoteOutcomes.yes)
 
     # now, get all the active ones...
-    active_wd = Watchdog.active(dashd)
+    active_wd = Watchdog.active(wavid)
     active_count = active_wd.count()
 
     # none exist, submit a new one to the network
@@ -42,7 +42,7 @@ def watchdog_check(dashd):
         # create/submit one
         printdbg("\tNo watchdogs exist... submitting new one.")
         wd = Watchdog(created_at=int(time.time()))
-        wd.submit(dashd)
+        wd.submit(wavid)
 
     else:
         wd_list = sorted(active_wd, key=lambda wd: wd.object_hash)
@@ -50,35 +50,35 @@ def watchdog_check(dashd):
         # highest hash wins
         winner = wd_list.pop()
         printdbg("\tFound winning watchdog [%s], voting VALID" % winner.object_hash)
-        winner.vote(dashd, VoteSignals.valid, VoteOutcomes.yes)
+        winner.vote(wavid, VoteSignals.valid, VoteOutcomes.yes)
 
         # if remaining Watchdogs exist in the list, vote delete
         for wd in wd_list:
             printdbg("\tFound losing watchdog [%s], voting DELETE" % wd.object_hash)
-            wd.vote(dashd, VoteSignals.delete, VoteOutcomes.yes)
+            wd.vote(wavid, VoteSignals.delete, VoteOutcomes.yes)
 
     printdbg("leaving watchdog_check")
 
 
-def prune_expired_proposals(dashd):
+def prune_expired_proposals(wavid):
     # vote delete for old proposals
-    for proposal in Proposal.expired(dashd.superblockcycle()):
-        proposal.vote(dashd, VoteSignals.delete, VoteOutcomes.yes)
+    for proposal in Proposal.expired(wavid.superblockcycle()):
+        proposal.vote(wavid, VoteSignals.delete, VoteOutcomes.yes)
 
 
-# ping dashd
-def sentinel_ping(dashd):
+# ping wavid
+def sentinel_ping(wavid):
     printdbg("in sentinel_ping")
 
-    dashd.ping()
+    wavid.ping()
 
     printdbg("leaving sentinel_ping")
 
 
-def attempt_superblock_creation(dashd):
-    import dashlib
+def attempt_superblock_creation(wavid):
+    import wavilib
 
-    if not dashd.is_masternode():
+    if not wavid.is_masternode():
         print("We are not a Masternode... can't submit superblocks!")
         return
 
@@ -89,7 +89,7 @@ def attempt_superblock_creation(dashd):
     # has this masternode voted on *any* superblocks at the given event_block_height?
     # have we voted FUNDING=YES for a superblock for this specific event_block_height?
 
-    event_block_height = dashd.next_superblock_height()
+    event_block_height = wavid.next_superblock_height()
 
     if Superblock.is_voted_funding(event_block_height):
         # printdbg("ALREADY VOTED! 'til next time!")
@@ -97,20 +97,20 @@ def attempt_superblock_creation(dashd):
         # vote down any new SBs because we've already chosen a winner
         for sb in Superblock.at_height(event_block_height):
             if not sb.voted_on(signal=VoteSignals.funding):
-                sb.vote(dashd, VoteSignals.funding, VoteOutcomes.no)
+                sb.vote(wavid, VoteSignals.funding, VoteOutcomes.no)
 
         # now return, we're done
         return
 
-    if not dashd.is_govobj_maturity_phase():
+    if not wavid.is_govobj_maturity_phase():
         printdbg("Not in maturity phase yet -- will not attempt Superblock")
         return
 
-    proposals = Proposal.approved_and_ranked(proposal_quorum=dashd.governance_quorum(), next_superblock_max_budget=dashd.next_superblock_max_budget())
-    budget_max = dashd.get_superblock_budget_allocation(event_block_height)
-    sb_epoch_time = dashd.block_height_to_epoch(event_block_height)
+    proposals = Proposal.approved_and_ranked(proposal_quorum=wavid.governance_quorum(), next_superblock_max_budget=wavid.next_superblock_max_budget())
+    budget_max = wavid.get_superblock_budget_allocation(event_block_height)
+    sb_epoch_time = wavid.block_height_to_epoch(event_block_height)
 
-    sb = dashlib.create_superblock(proposals, event_block_height, budget_max, sb_epoch_time)
+    sb = wavilib.create_superblock(proposals, event_block_height, budget_max, sb_epoch_time)
     if not sb:
         printdbg("No superblock created, sorry. Returning.")
         return
@@ -118,12 +118,12 @@ def attempt_superblock_creation(dashd):
     # find the deterministic SB w/highest object_hash in the DB
     dbrec = Superblock.find_highest_deterministic(sb.hex_hash())
     if dbrec:
-        dbrec.vote(dashd, VoteSignals.funding, VoteOutcomes.yes)
+        dbrec.vote(wavid, VoteSignals.funding, VoteOutcomes.yes)
 
         # any other blocks which match the sb_hash are duplicates, delete them
         for sb in Superblock.select().where(Superblock.sb_hash == sb.hex_hash()):
             if not sb.voted_on(signal=VoteSignals.funding):
-                sb.vote(dashd, VoteSignals.delete, VoteOutcomes.yes)
+                sb.vote(wavid, VoteSignals.delete, VoteOutcomes.yes)
 
         printdbg("VOTED FUNDING FOR SB! We're done here 'til next superblock cycle.")
         return
@@ -131,24 +131,24 @@ def attempt_superblock_creation(dashd):
         printdbg("The correct superblock wasn't found on the network...")
 
     # if we are the elected masternode...
-    if (dashd.we_are_the_winner()):
+    if (wavid.we_are_the_winner()):
         printdbg("we are the winner! Submit SB to network")
-        sb.submit(dashd)
+        sb.submit(wavid)
 
 
-def check_object_validity(dashd):
+def check_object_validity(wavid):
     # vote (in)valid objects
     for gov_class in [Proposal, Superblock]:
         for obj in gov_class.select():
-            obj.vote_validity(dashd)
+            obj.vote_validity(wavid)
 
 
-def is_dashd_port_open(dashd):
+def is_wavid_port_open(wavid):
     # test socket open before beginning, display instructive message to MN
     # operators if it's not
     port_open = False
     try:
-        info = dashd.rpc_command('getgovernanceinfo')
+        info = wavid.rpc_command('getgovernanceinfo')
         port_open = True
     except (socket.error, JSONRPCException) as e:
         print("%s" % e)
@@ -157,21 +157,21 @@ def is_dashd_port_open(dashd):
 
 
 def main():
-    dashd = DashDaemon.from_dash_conf(config.dash_conf)
+    wavid = WaviDaemon.from_wavi_conf(config.wavi_conf)
     options = process_args()
 
-    # check dashd connectivity
-    if not is_dashd_port_open(dashd):
-        print("Cannot connect to dashd. Please ensure dashd is running and the JSONRPC port is open to Sentinel.")
+    # check wavid connectivity
+    if not is_wavid_port_open(wavid):
+        print("Cannot connect to wavid. Please ensure wavid is running and the JSONRPC port is open to Sentinel.")
         return
 
-    # check dashd sync
-    if not dashd.is_synced():
-        print("dashd not synced with network! Awaiting full sync before running Sentinel.")
+    # check wavid sync
+    if not wavid.is_synced():
+        print("wavid not synced with network! Awaiting full sync before running Sentinel.")
         return
 
     # ensure valid masternode
-    if not dashd.is_masternode():
+    if not wavid.is_masternode():
         print("Invalid Masternode Status, cannot continue.")
         return
 
@@ -203,22 +203,22 @@ def main():
     # ========================================================================
     #
     # load "gobject list" rpc command data, sync objects into internal database
-    perform_dashd_object_sync(dashd)
+    perform_wavid_object_sync(wavid)
 
-    if dashd.has_sentinel_ping:
-        sentinel_ping(dashd)
+    if wavid.has_sentinel_ping:
+        sentinel_ping(wavid)
     else:
         # delete old watchdog objects, create a new if necessary
-        watchdog_check(dashd)
+        watchdog_check(wavid)
 
     # auto vote network objects as valid/invalid
-    # check_object_validity(dashd)
+    # check_object_validity(wavid)
 
     # vote to delete expired proposals
-    prune_expired_proposals(dashd)
+    prune_expired_proposals(wavid)
 
     # create a Superblock if necessary
-    attempt_superblock_creation(dashd)
+    attempt_superblock_creation(wavid)
 
     # schedule the next run
     Scheduler.schedule_next_run()
