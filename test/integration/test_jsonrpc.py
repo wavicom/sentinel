2d086234b3c6d4c7116bc97d8fa0f08ec51e8756
wavicom <40639092+wavicom@users.noreply.github.com>
2018-06-28 20:27:47 +0500

    Update test_jsonrpc.py

--- test/integration/test_jsonrpc.py
+++ test/integration/test_jsonrpc.py
@@ -8,30 +8,30 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'lib'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 import config
 
-from dashd import DashDaemon
-from dash_config import DashConfig
+from wavid import WaviDaemon
+from wavi_config import WaviConfig
 
 
-def test_dashd():
-    config_text = DashConfig.slurp_config_file(config.dash_conf)
+def test_wavid():
+    config_text = WaviConfig.slurp_config_file(config.wavi_conf)
     network = 'mainnet'
     is_testnet = False
-    genesis_hash = u'00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6'
+    genesis_hash = u'00000d39b78c04cc35653abe2442a106a428c5e92ba41ec4967a80a09abf4725'
     for line in config_text.split("\n"):
         if line.startswith('testnet=1'):
             network = 'testnet'
             is_testnet = True
             genesis_hash = u'00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c'
 
-    creds = DashConfig.get_rpc_creds(config_text, network)
-    dashd = DashDaemon(**creds)
-    assert dashd.rpc_command is not None
+    creds = WaviConfig.get_rpc_creds(config_text, network)
+    wavid = WaviDaemon(**creds)
+    assert wavid.rpc_command is not None
 
-    assert hasattr(dashd, 'rpc_connection')
+    assert hasattr(wavid, 'rpc_connection')
 
-    # Dash testnet block 0 hash == 00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c
+    # Wavi testnet block 0 hash == 00000d39b78c04cc35653abe2442a106a428c5e92ba41ec4967a80a09abf4725
     # test commands without arguments
-    info = dashd.rpc_command('getinfo')
+    info = wavid.rpc_command('getinfo')
     info_keys = [
         'blocks',
         'connections',
@@ -48,4 +48,4 @@ def test_dashd():
     assert info['testnet'] is is_testnet
 
     # test commands with args
-    assert dashd.rpc_command('getblockhash', 0) == genesis_hash
+    assert wavid.rpc_command('getblockhash', 0) == genesis_hash
