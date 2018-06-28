51ab69148fd049e243e6d26d70255ee34a0b3bc9
wavicom <40639092+wavicom@users.noreply.github.com>
2018-06-28 20:24:11 +0500

    Update travis_setup.sh

--- share/travis_setup.sh
+++ share/travis_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -evx
 
-mkdir ~/.dashcore
+mkdir ~/.wavicore
 
 # safety check
-if [ ! -f ~/.dashcore/.dash.conf ]; then
-  cp share/dash.conf.example ~/.dashcore/dash.conf
+if [ ! -f ~/.wavicore/.wavi.conf ]; then
+  cp share/wavi.conf.example ~/.wavicore/wavi.conf
 fi
