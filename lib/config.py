518f6f8b5d406e7fc73ca085ead7af3c047c0f34
wavicom <40639092+wavicom@users.noreply.github.com>
2018-06-28 19:40:51 +0500

    Update config.py

--- lib/config.py
+++ lib/config.py
@@ -3,27 +3,27 @@
 """
 import sys
 import os
-from dash_config import DashConfig
+from wavi_config import WaviConfig
 
 default_sentinel_config = os.path.normpath(
     os.path.join(os.path.dirname(__file__), '../sentinel.conf')
 )
 sentinel_config_file = os.environ.get('SENTINEL_CONFIG', default_sentinel_config)
-sentinel_cfg = DashConfig.tokenize(sentinel_config_file)
+sentinel_cfg = WaviConfig.tokenize(sentinel_config_file)
 sentinel_version = "1.1.0"
-min_dashd_proto_version_with_sentinel_ping = 70207
+min_wavid_proto_version_with_sentinel_ping = 70207
 
 
-def get_dash_conf():
+def get_wavi_conf():
     home = os.environ.get('HOME')
 
-    dash_conf = os.path.join(home, ".dashcore/dash.conf")
+    wavi_conf = os.path.join(home, ".wavicore/wavi.conf")
     if sys.platform == 'darwin':
-        dash_conf = os.path.join(home, "Library/Application Support/DashCore/dash.conf")
+        wavi_conf = os.path.join(home, "Library/Application Support/WaviCore/wavi.conf")
 
-    dash_conf = sentinel_cfg.get('dash_conf', dash_conf)
+    wavi_conf = sentinel_cfg.get('wavi_conf', wavi_conf)
 
-    return dash_conf
+    return wavi_conf
 
 
 def get_network():
@@ -79,6 +79,6 @@ def get_db_conn():
     return db
 
 
-dash_conf = get_dash_conf()
+wavi_conf = get_wavi_conf()
 network = get_network()
 db = get_db_conn()
