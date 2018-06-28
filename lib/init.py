6e560fc2e046026da5bd96c82db3764fc1bc24ae
wavicom <40639092+wavicom@users.noreply.github.com>
2018-06-28 19:46:47 +0500

    Update init.py

--- lib/init.py
+++ lib/init.py
@@ -57,23 +57,23 @@ def is_database_correctly_configured():
     return configured
 
 
-def has_dash_conf():
+def has_wavi_conf():
     import config
     import io
 
-    valid_dash_conf = False
+    valid_wavi_conf = False
 
-    # ensure dash_conf exists & readable
+    # ensure wavi_conf exists & readable
     #
-    # if not, print a message stating that Dash Core must be installed and
-    # configured, including JSONRPC access in dash.conf
+    # if not, print a message stating that WaviCore must be installed and
+    # configured, including JSONRPC access in wavi.conf
     try:
-        f = io.open(config.dash_conf)
-        valid_dash_conf = True
+        f = io.open(config.wavi_conf)
+        valid_wavi_conf = True
     except IOError as e:
         print(e)
 
-    return valid_dash_conf
+    return valid_wavi_conf
 
 
 # === begin main
@@ -95,8 +95,8 @@ def main():
         print("Please ensure correct database configuration.")
         sys.exit(1)
 
-    if not has_dash_conf():
-        print("DashCore must be installed and configured, including JSONRPC access in dash.conf")
+    if not has_wavi_conf():
+        print("WaviCore must be installed and configured, including JSONRPC access in wavi.conf")
         sys.exit(1)
 
 
